919a2087196adf42fc89a70d5243160bc8fce135
Peter Elmered <peter@elmered.com>
2014-09-18 21:53:48 +0200

    Updated version to 0.1.6

--- includes/pricefiles.php
+++ includes/pricefiles.php
@@ -19,7 +19,7 @@ class WC_Pricefiles
      * @since    0.1.0
      * @var     string
      */
-    const VERSION = '0.1.5';
+    const VERSION = '0.1.6';
 
     /**
      * Unique identifier for your plugin.
